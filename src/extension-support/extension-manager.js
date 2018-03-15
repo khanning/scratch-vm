713f587b8b8e95bcd15c8cf73ffdabf9c0767dce
Kreg Hanning <khanning@gmail.com>
2018-03-15 17:48:42 -0400

    Add micro:bit web-bt extension

--- src/extension-support/extension-manager.js
+++ src/extension-support/extension-manager.js
@@ -9,10 +9,12 @@ const BlockType = require('./block-type');
 const Scratch3PenBlocks = require('../extensions/scratch3_pen');
 const Scratch3WeDo2Blocks = require('../extensions/scratch3_wedo2');
 const Scratch3MusicBlocks = require('../extensions/scratch3_music');
+const Scratch3MicroBitBlocks = require('../extensions/scratch3_microbit');
 const builtinExtensions = {
     pen: Scratch3PenBlocks,
     wedo2: Scratch3WeDo2Blocks,
-    music: Scratch3MusicBlocks
+    music: Scratch3MusicBlocks,
+    microbit: Scratch3MicroBitBlocks
 };
 
 /**
